f9b8af96ae7568d66be6c6b0c5f745121624fd2d
azhigov <adam.azhigov97@gmail.com>
2021-04-06 18:03:55 +0300

    task7

---
+++ object.js
@@ -0,0 +1,27 @@
+let phone = {
+    brend: 'Honor',
+    model: '9 lite',
+    phoneStatus: false,
+    call: function (name){
+        return `я звоню ${name}`
+    },
+    batteryPower: '50',
+    getBatteryPower: function (){
+        return `ваша зарядка ${this.batteryPower}%`
+    },
+    isOn: function (){
+        if (this.phoneStatus == true){
+            return 'Телефон включен'
+        } else {
+            return 'Телефон выключен'
+        }
+    },
+    switch: function (){
+        if (this.phoneStatus = !this.phoneStatus){
+
+        }
+
+    }
+}
+console.log(phone.switch())
+console.log(phone.phoneStatus)
